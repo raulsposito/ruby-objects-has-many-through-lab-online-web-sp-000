34eadb79006938ab557b40ef7e13ab68be1d6748
Raul Sposito <raulsposito@gmail.com>
2019-09-24 12:52:01 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,8 +13,8 @@ class Doctor
     @@all << self
   end
 
-  def new_appointment(date, patient)
-    Appointment.new(date, self, patient)
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
   end
   
   def appointments 
