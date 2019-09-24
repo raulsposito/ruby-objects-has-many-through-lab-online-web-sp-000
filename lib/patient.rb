618d2d28bfd93bf82f4fa7064532095b2ca2b812
Raul Sposito <raulsposito@gmail.com>
2019-09-24 12:54:27 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,32 +1,26 @@
-class Patient 
-  
-  attr_accessor :name 
-  
+lass Patient
+  attr_accessor :name
+
   @@all = []
-  
+
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self
   end
-  
-  def new_appointment(date, doctor)
-    Appointment.new(date, doctor, self)
-  end
-  
-  def appointments 
-    Appointment.all.select do |appointment|
-      appointment.patient == self 
-    end
-  end
-  
-  def doctors 
-    appointments.map do |appointment|
-      appointment.doctor 
-    end
-  end
-  
+
   def self.all
-    @@all 
+    @@all
   end
-  
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.patient == self }
+  end
+
+  def doctors
+    appointments.map(&:doctor)
+  end
 end
